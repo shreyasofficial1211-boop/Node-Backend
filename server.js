5ae14362fb9b0d1cc8fd951b249a41b98ac69929
XAI hub <shreyas.official.12.11@gmail.com>
2025-09-26 12:08:25 +0530

    Prepare Backen

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const DATA_FILE = 'data.json';
+
+// POST: Add new user
+app.post('/users', (req, res) => {
+  const newUser = req.body;
+
+  let users = [];
+  if (fs.existsSync(DATA_FILE)) {
+    users = JSON.parse(fs.readFileSync(DATA_FILE));
+  }
+
+  newUser.id = users.length + 1;
+  users.push(newUser);
+
+  fs.writeFileSync(DATA_FILE, JSON.stringify(users, null, 2));
+
+  res.status(201).json(newUser);
+});
+
+// GET: View all users
+app.get('/users', (req, res) => {
+  if (fs.existsSync(DATA_FILE)) {
+    const users = JSON.parse(fs.readFileSync(DATA_FILE));
+    res.json(users);
+  } else {
+    res.json([]);
+  }
+});
+
+app.listen(3000, () => console.log('✅ Server running on http://localhost:3000'));
